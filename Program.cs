d3e901243664a48536b98f63857c534c606decd7
hamzazahid8556205 <129792956+hamzazahid8556205@users.noreply.github.com>
2024-08-07 04:50:30 +0500

    Initial commit of Math Operations Project

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MathOperations
+{
+    
+    public class MathOps
+    {
+        
+        public void PerformOperation(int num1, int num2)
+        {
+           
+            int result = num1 * num1;
+
+            Console.WriteLine("The second integer is: " + num2);
+            Console.WriteLine("The result of the math operation on the first integer is: " + result);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            MathOps mathOps = new MathOps();
+
+          
+            mathOps.PerformOperation(5, 10);
+
+           
+            mathOps.PerformOperation(num1: 7, num2: 14);
+
+            
+            Console.ReadLine();
+        }
+    }
+}
