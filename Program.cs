67c379b0c37391e546b678332206c11455bacf1e
hamzazahid8556205 <129792956+hamzazahid8556205@users.noreply.github.com>
2024-08-07 04:46:05 +0500

    Initial commit of Entity Framework Project

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Entity_Framework2
+{
+   
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+    }
+
+
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SchoolContext())
+            {
+                var student = new Student { Name = "John Doe" };
+                context.Students.Add(student);
+                context.SaveChanges();
+
+                Console.WriteLine("Student saved to the database.");
+            }
+
+            Console.ReadKey(); 
+        }
+    }
+}
+
