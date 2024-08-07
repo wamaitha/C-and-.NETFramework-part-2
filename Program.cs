b77817d0428bd5a468a1a66d58ce5fa080906005
hamzazahid8556205 <129792956+hamzazahid8556205@users.noreply.github.com>
2024-08-07 05:00:04 +0500

    Initial commit of Polymorphism Assignment Project

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace polymorphism_assignment
+{
+  
+    public interface IQuitable
+    {
+        void Quit();
+    }
+
+    
+    public class Employee : IQuitable
+    {
+        public string Name { get; set; }
+
+        public Employee(string name)
+        {
+            Name = name;
+        }
+
+        
+        public void Quit()
+        {
+            Console.WriteLine($"{Name} has quit the job.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuitable employee = new Employee("John Doe");
+            employee.Quit();
+
+            Console.ReadLine();
+        }
+    }
+}
